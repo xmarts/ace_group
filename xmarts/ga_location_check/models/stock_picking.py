a6a63e4fa2842fe3339b3102f03914ec4d2acd76
Luis Muro <luis.muro@xmarts.com>
2022-05-24 17:17:04 -0500

    [ADD] update

--- xmarts/ga_location_check/models/stock_picking.py
+++ xmarts/ga_location_check/models/stock_picking.py
@@ -12,6 +12,21 @@ _logger = logging.getLogger(__name__)
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
+    def button_validate(self):
+        for rec in self:
+            location = False
+            if rec.picking_type_id=='internal' and rec.location_dest_id.ga_is_check_package_done:
+                location = rec.location_dest_id
+            if rec.picking_type_id=='incoming' and rec.location_dest_id.ga_is_check_package_done:
+                location = rec.location_dest_id
+            if rec.picking_type_id=='outgoing' and rec.location_id.ga_is_check_package_done:
+                location = rec.location_id
+            if location and rec.packages_level_ids_details:
+                if any(x.is_done for x in rec.packages_level_ids_details):
+                    raise ValidationError("Error. Este almacen esta restringido a no procesar packages done")
+                    return False        
+        return super(StockPicking, self).button_validate()
+
     @api.constrains("state")
     def check_package_done(self):
         for rec in self:
